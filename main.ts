64b39b82afdf296e2204a294e331fdd61baad648
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-03-08 21:54:13 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-pins.touchSetMode(TouchTarget.LOGO, TouchTargetMode.Capacitive)
-servos.P1.setAngle(90)
-servos.P2.setAngle(90)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+servos.P0.setAngle(135)
+servos.P1.setAngle(135)
 basic.forever(function () {
-    while (input.logoIsPressed()) {
-        servos.P1.setAngle(180)
-        servos.P2.setAngle(180)
+    while (input.pinIsPressed(TouchPin.P2)) {
+        servos.P0.setAngle(45)
+        servos.P1.setAngle(45)
         basic.pause(200)
-        servos.P1.setAngle(90)
-        servos.P2.setAngle(90)
+        servos.P0.setAngle(135)
+        servos.P1.setAngle(135)
         basic.pause(200)
     }
 })
