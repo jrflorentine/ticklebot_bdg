ec0e4c34d6fd143795751339c23a90b94fea7d50
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-03-09 19:00:59 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ basic.forever(function () {
     while (input.pinIsPressed(TouchPin.P2)) {
         servos.P0.setAngle(45)
         servos.P1.setAngle(45)
-        basic.pause(200)
+        basic.pause(250)
         servos.P0.setAngle(135)
         servos.P1.setAngle(135)
-        basic.pause(200)
+        basic.pause(250)
     }
 })
